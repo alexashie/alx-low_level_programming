11e674bda53cba9565db92bc5f46e88d667ab40e
alexashie <110497218+alexashie@users.noreply.github.com>
2023-08-10 11:14:06 +0400

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
  * Description: 'prints size of different data types'
- * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
