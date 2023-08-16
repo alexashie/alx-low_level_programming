117aa586384c88990315416ca5daf1b202c8029d
alexashie <110497218+alexashie@users.noreply.github.com>
2023-08-16 11:07:31 +0000

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
+#include"main.h"
+
 /**
  * main - Entry point
  * Description: 'prints _putchar followed by new line'
  * Return: Always 0 (Success)
  */
 
-int	main(void)
+int main(void)
 {
-	write(1, "_putchar", 8);
-	write(1, "\n", 1);
-	return(0);
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * print_alphabet
+*/
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		_putchar(c);
+	_putchar('\n');
+}
